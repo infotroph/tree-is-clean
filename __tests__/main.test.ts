d07967e71dcb86123530161f194e1a37970efa2b
Chris Black <chris@ckblack.org>
2020-02-16 22:20:31 +0100

    convert template to do something useful

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,27 +1,107 @@
-import {wait} from '../src/wait'
 import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
+import * as fs from 'fs'
+import {tmpdir} from 'os'
 
-test('throws invalid number', async () => {
-  const input = parseInt('foo', 10)
-  await expect(wait(input)).rejects.toThrow('milliseconds not a number')
+let test_dir = ''
+
+// TODO this is a lot of setup for tests that really only parse stdout.
+// consider mocking exec.exec to produce fake Git output instead
+beforeAll(() => {
+  try {
+    test_dir = fs.mkdtempSync(
+      path.join(fs.realpathSync(tmpdir()), 'treeclean_tmp')
+    )
+    process.chdir(test_dir)
+    cp.execSync('git init')
+    fs.writeFileSync('a.txt', 'abcde')
+    cp.execSync('git add .')
+    cp.execSync('git commit -m "first"')
+  } catch (e) {
+    console.log(e)
+  }
 })
 
-test('wait 500 ms', async () => {
-  const start = new Date()
-  await wait(500)
-  const end = new Date()
-  var delta = Math.abs(end.getTime() - start.getTime())
-  expect(delta).toBeGreaterThan(450)
+afterAll(() => {
+  fs.rmdirSync(test_dir, {recursive: true})
 })
 
-// shows how the runner will run a javascript action with env / stdout protocol
-test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = '500'
+beforeEach(() => {
+  try {
+    cp.execSync('git checkout -- .')
+  } catch (e) {
+    console.log(e)
+  }
+})
+
+function execute_action() {
   const ip = path.join(__dirname, '..', 'lib', 'main.js')
   const options: cp.ExecSyncOptions = {
     env: process.env
   }
-  console.log(cp.execSync(`node ${ip}`, options).toString())
+
+  // cp.execSync throws if cmd status not zero,
+  // but we want to inspect stdout in either case
+  try {
+    return {
+      status: 0,
+      msg: cp.execSync(`node ${ip}`, options).toString()
+    }
+  } catch (e) {
+    return {
+      status: e.status,
+      msg: e.stdout.toString()
+    }
+  }
+}
+
+test('clean env', () => {
+  var out = execute_action()
+  expect(out.status).toBe(0)
+  expect(out.msg).toMatch(new RegExp('^\\[command\\].*--porcelain\\s*$'))
+})
+
+test('modified file', () => {
+  fs.appendFileSync('a.txt', 'fghi')
+  var out = execute_action()
+  expect(out.status).not.toBe(0)
+  expect(out.msg).toMatch('::error::')
+  expect(out.msg).toMatch('M a.txt')
+})
+
+test('new file', () => {
+  // want consider-untracked to default to true
+  fs.writeFileSync('b.txt', 'this is a new file')
+  var out = execute_action()
+  expect(out.status).not.toBe(0)
+  expect(out.msg).toMatch('::error::')
+  expect(out.msg).toMatch('?? b.txt')
+
+  process.env['INPUT_CONSIDER-UNTRACKED'] = 'true'
+  out = execute_action()
+  expect(out.status).not.toBe(0)
+  expect(out.msg).toMatch('::error::')
+  expect(out.msg).toMatch('?? b.txt')
+
+  process.env['INPUT_CONSIDER-UNTRACKED'] = 'false'
+  out = execute_action()
+  expect(out.status).toBe(0)
+  expect(out.msg).not.toMatch('::error::')
+  expect(out.msg).not.toMatch('b.txt')
+})
+
+test('new plus modified', () => {
+  fs.writeFileSync('b.txt', 'ignorable content here')
+  fs.appendFileSync('a.txt', 'jklm')
+
+  process.env['INPUT_CONSIDER-UNTRACKED'] = 'true'
+  var out = execute_action()
+  expect(out.status).not.toBe(0)
+  expect(out.msg).toMatch(new RegExp('M a.txt\\s+\\?\\? b.txt\\s+::error::'))
+
+  process.env['INPUT_CONSIDER-UNTRACKED'] = 'false'
+  out = execute_action()
+  expect(out.status).not.toBe(0)
+  expect(out.msg).toMatch(new RegExp('M a.txt\\s+::error::'))
 })
